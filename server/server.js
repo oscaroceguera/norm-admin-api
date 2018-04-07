d156f25335140948e771d9f88157558c9a17d03b
oscaroceguera <oscaroceguera@gmail.com>
2018-04-07 03:39:12 -0500

    added morgan

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 require('./config/config')
 
 const express = require('express')
+const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const { mongoose } = require('./db/mongoose')
 const cors = require('cors')
 
 const app = express()
+app.use(morgan('combined'))
 app.use(cors())
 
 const port = process.env.PORT
